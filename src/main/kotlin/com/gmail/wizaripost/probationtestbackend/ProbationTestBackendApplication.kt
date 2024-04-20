6089a65558e08bf4876cd2967b8c207cd5db8af7
wizari <wizaripost@gmail.com>
2024-04-20 17:17:35 +0300

    Initial

---
+++ src/main/kotlin/com/gmail/wizaripost/probationtestbackend/ProbationTestBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.gmail.wizaripost.probationtestbackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ProbationTestBackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<ProbationTestBackendApplication>(*args)
+}
---
+++ src/test/kotlin/com/gmail/wizaripost/probationtestbackend/ProbationTestBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.gmail.wizaripost.probationtestbackend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ProbationTestBackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
